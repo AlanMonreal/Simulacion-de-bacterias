f0930c7615135052914b2e944e5bf80755e4733f
Uriel Robles <uriel_r@outlook.com>
2019-04-23 15:03:41 -0500

    clase bacteria

--- Bacteria.py
+++ Bacteria.py
@@ -1,3 +1,24 @@
-class Bacteria():
-	def __init__(self):
-		pass
+import pygame, sys
+from pygame.locals import *
+import random
+
+class Bacteria(pygame.sprite.Sprite):
+	def __init__(self, position_x, position_y, gram=True):
+		""" posicion de la bacteria en la simulacion
+			gram: definir si la bacteria es gram positiva:True o negativa:False"""
+		pygame.sprite.Sprite.__init__(self)
+
+		#sprite de la bacteria
+		self.sprite = pygame.image.load("sprites/bacteria.png")
+		self.sprite_de_bacteria = self.sprite.get_rect()
+
+		#posicion de la bacteria
+		self.sprite_de_bacteria.centerx = position_x
+		self.sprite_de_bacteria.centery = position_y
+		self.gram = gram
+		self.salud = 100
+		self.energia = 100
+		self.reproduccion = False
+
+	def colocar_bacteria(self, window):
+		window.blit(self.sprite, self.sprite_de_bacteria)
--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 import pygame, sys
-
+from Bacteria import Bacteria
 from pygame.locals import *
+import random
 
 pygame.init()
 pygame.display.set_caption("Simulacion bacterias")
 
-window = pygame.display.set_mode((1200, 700))
+window = pygame.display.set_mode((700, 700))
+
+
+clock = pygame.time.Clock()
 
 def simulation():
     while True:
-        window.fill((0, 0, 0))
+        bacterias = [Bacteria(random.randint(0, 700), random.randint(0, 700)) for i in range(20)]
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
-if __name__ == '__main__':
-    simulation()
+
+        window.fill((255, 255, 255))
+        for bacteria in bacterias:
+            bacteria.colocar_bacteria(window)
+            bacteria.update()
+        clock.tick(6)
+        pygame.display.flip()
+
+if __name__ == '__main__': simulation()
