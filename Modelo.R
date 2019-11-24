f42e98f8dd46845d4fda66f0f7957572772ed499
AlanMonreal <alan_monrealan@outlook.com>
2019-11-23 21:13:21 -0600

    Added uses R function dynamically

--- Modelo.R
+++ Modelo.R
@@ -14,40 +14,40 @@ library(deSolve)
 
 # Declaramos la funcion metiendo nuestro sistema de ecuaciones
 ModeloTuberculosis<- function(t, y, parms){  
-  MNA_t=y[1];# Macrofago no activado 
-  MA_t=y[2];# Macrofago activado
-  MNAF_t=y[3];# Macrofago no activado que fagocito
-  MAF_t=y[4];# Macrofago activado que fagocito
-  TL_t=y[5];# Tuberculosis libre
-  TNA_t=y[6];# Tuberculosis en MNAF
+  MNA_t=y[1]# Macrofago no activado 
+  MA_t=y[2]# Macrofago activado
+  MNAF_t=y[3]# Macrofago no activado que fagocito
+  MAF_t=y[4]# Macrofago activado que fagocito
+  TL_t=y[5]# Tuberculosis libre
+  TNA_t=y[6]# Tuberculosis en MNAF
   TA_t=y[7]# Tuberculosis en MAF
   
   dMNA<- -MNA_t*(parms[11]+parms[13]*TL_t)# Vida media de macrófago no activado y muerte por M. tuberculosis libre
   -MNA_t*parms[9]# Activación de macrófago no activado
   -MNA_t*TL_t*parms[7]# Fagocitación de M. tuberculosis libre por macrófago no activado
   +MNAF_t*parms[5]+MAF_t*parms[6]# Llegada de macrófago no activado por mecanismos pro-inflamatorios
-  +MNA_t*parms[4];# Entrada constante de macrófago no activado
+  +MNA_t*parms[4]# Entrada constante de macrófago no activado
  
   dMA<- -MA_t*(parms[14]+parms[16]*TL_t)# Vida media de macrófago activado y muerte por M. tuberculosis libre 
   -MA_t*TL_t*parms[8]# Fagocitación de M. tuberculosis libre por macrófago activado
-  +MNA_t*parms[9];# Activación de macrófago no activado
+  +MNA_t*parms[9]# Activación de macrófago no activado
   
   dMNAF<- -MNAF_t*(parms[17]+parms[20]*TL_t+parms[21]*TNA_t)# Vida media de macrófago no activado que fagocitó y muerte por M. tuberculosis libre y fagocitada 
   -MNAF_t*parms[10]# Activación de macrófago no activado que fagocitó
-  +MNA_t*TL_t*parms[7];# Fagocitación de M. tuberculosis libre por macrófago no activado
+  +MNA_t*TL_t*parms[7]# Fagocitación de M. tuberculosis libre por macrófago no activado
   
   dMAF<- -MAF_t*(parms[22]+parms[25]*TL_t+parms[26]*TA_t)# Vida media de macrófago activado que fagocitó y muerte por M. tuberculosis libre y fagocitada
   +MNAF_t*parms[10]# Vida media de macrófago activado que fagocitó y muerte por M. tuberculosis libre y fagocitada 
-  +MA_t*TL_t*parms[8];# Fagocitación de M. tuberculosis libre por macrófago activado
+  +MA_t*TL_t*parms[8]# Fagocitación de M. tuberculosis libre por macrófago activado
   
   dTL<- +TL_t*parms[1]# Proliferación de M. Tuberculosis libre
   -TL_t*(parms[27]+parms[12]*MNA_t+parms[15]*MA_t+parms[18]*MNAF_t+parms[23]*MAF_t)# Vida media de M. Tuberculosis libre y muerte por todos los macrófagos que hay en el sistema
   -MA_t*TL_t*parms[8]# Fagocitación de M. Tuberculosis libre por macrófago activo
-  -MNA_t*TL_t*parms[7];# Fagocitación de M. Tuberculosis libre por macrófago no activo
+  -MNA_t*TL_t*parms[7]# Fagocitación de M. Tuberculosis libre por macrófago no activo
   
   dTNA<- +TNA_t*parms[2]# Proliferación de M. Tuberculosis en macrófago no activo
   -TNA_t*(parms[28]+parms[19]*MNAF_t)# Vida media de M. Tuberculosis en M.N.A.F. y muerte por macrófago no activo que fagocitó
-  +MNA_t*TL_t*parms[7];# Fagocitación de M. Tuberculosis libre por macrófago no activo
+  +MNA_t*TL_t*parms[7]# Fagocitación de M. Tuberculosis libre por macrófago no activo
   
   dTA<- +TA_t*parms[3]# Proliferación de M. Tuberculosis en macrófago activo
   -TA_t*(parms[29]+parms[24]*MAF_t)# Vida media de M. Tuberculosis en M.A.F. y muerte por macrófago activo que fagocitó
@@ -60,104 +60,105 @@ ModeloTuberculosis<- function(t, y, parms){
 # Declaramos los valores de los parametros
 
 # aTl - Tasa de proliferacion de M. tuberculosis
-k1=3;
+k1=3
 
 # aTna - Tasa de proliferacion de M. tuberculosis fagocitada por macrofago no activado
-k2=24;
+k2=24
 
 # aTa - Tasa de proliferacion de M. tuberculosis fagocitada por macrofago activado
-k3=0.625;
+k3=0.625
 
 # aM - Entrada constante de macrofago
-k4=0.1;
+k4=0.1
 
 # aMna - Tasa de reclutamiento de monocitos circulantes al lugar de la infección por Macrófago no activado que fagocitó
-k5=1;
+k5=1
 
 # aMa - Tasa de reclutamiento de monocitos circulantes al lugar de la infección por Macrófago activado que fagocitó 
-k6=3;
+k6=3
 
 # yMna - Capacidad fagocítica de Macrófago no activado
-k7=.75;
+k7=.75
 
 # yMa - Capacidad fagocítica de Macrófago activado
-k8=0.35;
+k8=0.35
 
 # 0.650 uM - Activación de macrófago que no ha fagocitado
-k9=0.650;
+k9=0.650
 
 # uMf - Activación de macrófago que ha fagocitado
-k10=0.90;
+k10=0.90
 
 # dMna - Vida media de macrofago no activado
-k11=17;
+k11=17
 
 # dMnaTl - Tasa de muerte de M. tuberculosis libre causado por Macrófago no activado
-k12=48;
+k12=48
 
 # dTlMna - Tasa de muerte de Macrófago no activado causada por M. tuberculosis libre
-k13=2;
+k13=2
 
 # dMa - Vida media de macrofago activado
-k14=1;
+k14=1
 
 # dMaTl - Tasa de muerte de M. tuberculosis libre causado por Macrófago activado 
-k15=1;
+k15=1
 
 # dTlMa - Tasa de muerte de Macrófago activado causada por M. tuberculosis libre
-k16=2;
+k16=2
 
 # dMnaf - Vida media de Macrófago no activado que ha fagocitado
-k17=1;
+k17=1
 
 # dMnafTl - Tasa de muerte de M. tuberculosis libre causado por Macrófago no activado
-k18=12;
+k18=12
 
 # dMnafT - Tasa de muerte de M. tuberculosis fagocitada causado por Macrófago no activado
-k19=0.59;
+k19=0.59
 
 # dTlMnaf - Tasa de muerte de Macrófago no activado que ha fagocitado causada por M. tuberculosis libre
-k20=2;
+k20=2
 
 # 0.92 dTMnaf - Tasa de muerte de Macrófago no activado que ha fagocitado causada por M. tuberculosis fagocitada
-k21=0.92;
+k21=0.92
 
 # dMaf - Vida media de Macrófago activado que ha fagocitado
-k22=1.3;
+k22=1.3
 
 # dMafTl - Tasa de muerte de M. tuberculosis libre causado por Macrófago activado que ha fagocitado
-k23=5.3;
+k23=5.3
 
 # MafT - Tasa de muerte de M. tuberculosis fagocitada causado por Macrófago activado que ha fagocitado
-k24=0.34;
+k24=0.34
 
 # TlMaf - Tasa de muerte de Macrófago activado que ha fagocitado causada por M. tuberculosis libre
-k25=1.1;
+k25=1.1
 
 # TMaf - Tasa de muerte de Macrófago activado que ha fagocitado causada por M. tuberculosis fagocitada
-k26=19;
+k26=19
 
 # dTl - Vida media de M. tuberculosis libre
-k27=2.2;
+k27=2.2
 
 # dTna - Vida media de M. tuberculosis fagocitada por macrófago no activado
-k28=0.33;
+k28=0.33
 
 # dTa - Vida media de M. tuberculosis fagocitada por macrófago activado
-k29=7;
+k29=7
 
 
 parms=c(k1, k2, k3, k4, k5, k6, k7, k8, k9, k10, k11, k12, k13, k14, k15,
-        k16, k17, k18, k19, k20, k21, k22, k23, k24, k25, k26, k27, k28, k29);
+        k16, k17, k18, k19, k20, k21, k22, k23, k24, k25, k26, k27, k28, k29)
 
 # condiciones iniciales de los nodos de la red
-y0=c(5,15,4,2,2,6,3);
+y0=c(5,15,4,2,2,6,3)
 
 # Definimos el intervalo de integracion
 tspan =seq(from = 0, to = 12, by = 0.001)
 
 # Integramos con funcion ode
-out <- ode(y = y0, times = tspan, func = ModeloTuberculosis, parms = parms)
+out <- ode(y = y0, t = tspan, func = ModeloTuberculosis, parms = parms)
+# out <- ode(y = y0, times = tspan, func = ModeloTuberculosis, parms = parms)
 
 # Graficamos los resultados
 par(pty="s")
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import tkinter as tk
-import subprocess
 from single_mode import SingleMode
 from batch_mode import BatchMode
+# from rpy2.robjects.packages import STAP
 import rpy2.robjects as ro
+import numpy as np
+import csv
 
 
 class Welcome(tk.Frame):
@@ -30,14 +32,7 @@ class Welcome(tk.Frame):
     def launch_single_mode(self):
         self.single = SingleMode(self)
         self.single.wm_title("MODO SIMPLE")
-
-        # TODO send real args
-        # subprocess.call(['Rscript', 'Modelo.R', '1', '2'], shell=False)
-        r = ro.r
-        r['source']("Modelo.R")
-        print('done')
-        print(type(r.out))
-        # root.withdraw()
+        create_r_script()
 
     def launch_batch_mode(self):
         self.batch = BatchMode(self)
@@ -45,168 +40,148 @@ class Welcome(tk.Frame):
 
 
 def create_r_script():
-    r = ro.r('''
-        source("Grind.R")
-        library(deSolve)
+    # ModeloTuberculosis<- function(t, y, parms){
+    # y0 = c(5, 15, 4, 2, 2, 6, 3)
+    # parms = c(k1, k2, k3, k4, k5, k6, k7, k8, k9, k10, k11, k12, k13,
+    #           k14, k15, k16, k17, k18, k19, k20, k21, k22, k23, k24, k25,
+    #           k26, k27, k28, k29)
+    # k1 <- 3
+    # k2 <- 24
+    # k3 <- 0.625
+    # k4 <- 0.1
+    # k5 <- 1
+    # k6 <- 3
+    # k7 <- 0.75
+    # k8 <- 0.35
+    # k9 <- 0.650
+    # k10 <- 0.90
+    # k11 <- 17
+    # k12 <- 48
+    # k13 <- 2
+    # k14 <- 1
+    # k15 <- 1
+    # k16 <- 2
+    # k17 <- 1
+    # k18 <- 12
+    # k19 <- 0.59
+    # k20 <- 2
+    # k21 <- 0.92
+    # k22 <- 1.3
+    # k23 <- 5.3
+    # k24 <- 0.34
+    # k25 <- 1.1
+    # k26 <- 19
+    # k27 <- 2.2
+    # k28 <- 0.33
+    # k29 <- 7
+    # tspan = seq(from = 0, to = 12, by = 0.01)
+    print('creating R script')
+    ro.r('''
+            source("Grind.R")
+            library(deSolve)
 
-        # args = commandArgs(trailingOnly=TRUE)
-        # if(length(args)==0){
-        #   stop("No args", call.=FALSE)
-        # } else if (length(args) >= 1) {
-        #   print(args[0])
-        # }
+            model <- function(t, y, parms){
+                MNA_t  <- y[1]
+                MA_t   <- y[2]
+                MNAF_t <- y[3]
+                MAF_t  <- y[4]
+                TL_t   <- y[5]
+                TNA_t  <- y[6]
+                TA_t   <- y[7]
 
-        # Declaramos la funcion metiendo nuestro sistema de ecuaciones
-        ModeloTuberculosis<- function(t, y, parms){
-            MNA_t=y[1];
-            MA_t=y[2];
-            MNAF_t=y[3];
-            MAF_t=y[4];
-            TL_t=y[5];
-            TNA_t=y[6];
-            TA_t=y[7];
+                dMNA <- -MNA_t*(parms[11]+parms[13]*TL_t)
+                    -MNA_t*parms[9]
+                    -MNA_t*TL_t*parms[7]
+                    +MNAF_t*parms[5]+MAF_t*parms[6]
+                    +MNA_t*parms[4]
 
-            dMNA<- -MNA_t*(parms[11]+parms[13]*TL_t)
-            -MNA_t*parms[9]
-            -MNA_t*TL_t*parms[7]
-            +MNAF_t*parms[5]+MAF_t*parms[6]
-            +MNA_t*parms[4];
+                dMA <- -MA_t*(parms[14]+parms[16]*TL_t)
+                    -MA_t*TL_t*parms[8]
+                    +MNA_t*parms[9];
 
-            dMA<- -MA_t*(parms[14]+parms[16]*TL_t)
-            -MA_t*TL_t*parms[8]
-            +MNA_t*parms[9];
+                dMNAF <- -MNAF_t*(parms[17]+parms[20]*TL_t+parms[21]*TNA_t)
+                    -MNAF_t*parms[10]
+                    +MNA_t*TL_t*parms[7];
 
-            dMNAF<- -MNAF_t*(parms[17]+parms[20]*TL_t+parms[21]*TNA_t)
-            -MNAF_t*parms[10]
-            +MNA_t*TL_t*parms[7];
+                dMAF <- -MAF_t*(parms[22]+parms[25]*TL_t+parms[26]*TA_t)
+                    +MNAF_t*parms[10]
+                    +MA_t*TL_t*parms[8];
 
-            dMAF<- -MAF_t*(parms[22]+parms[25]*TL_t+parms[26]*TA_t)
-            +MNAF_t*parms[10]
-            +MA_t*TL_t*parms[8];
+                dTL <- +TL_t*parms[1]
+                    -TL_t*(parms[27]+parms[12]*MNA_t+parms[15]*MA_t+parms[18]*MNAF_t+parms[23]*MAF_t)
+                    -MA_t*TL_t*parms[8]
+                    -MNA_t*TL_t*parms[7];
 
-            dTL<- +TL_t*parms[1]
-            -TL_t*(parms[27]+parms[12]*MNA_t+parms[15]*MA_t+parms[18]*MNAF_t+parms[23]*MAF_t)
-            -MA_t*TL_t*parms[8]
-            -MNA_t*TL_t*parms[7];
+                dTNA <- +TNA_t*parms[2]
+                    -TNA_t*(parms[28]+parms[19]*MNAF_t)
+                    +MNA_t*TL_t*parms[7];
 
-            dTNA<- +TNA_t*parms[2]
-            -TNA_t*(parms[28]+parms[19]*MNAF_t)
-            +MNA_t*TL_t*parms[7];
+                dTA <- +TA_t*parms[3]
+                    -TA_t*(parms[29]+parms[24]*MAF_t)
+                    +MA_t*TL_t*parms[8]
+                    +MNA_t*parms[10]
 
-  dTA<- +TA_t*parms[3]# Proliferación de M. Tuberculosis en macrófago activo
-  -TA_t*(parms[29]+parms[24]*MAF_t)# Vida media de M. Tuberculosis en M.A.F. y muerte por macrófago activo que fagocitó
-  +MA_t*TL_t*parms[8]# Fagocitación de M. Tuberculosis libre por macrófago activo
-  +MNA_t*parms[10]# Activación de macrófago no activo
+                list(c(dMNA,dMA,dMNAF,dMAF,dTL,dTNA,dTA))
+            }
 
-  list(c(dMNA,dMA,dMNAF,dMAF,dTL,dTNA,dTA))
-}
+            run <- function(y, k){
+                parms = k
+                y0 = y
+                tspan = seq(from = 0, to = 5, by = 0.5)
+                out <- ode(y = y0, times = tspan, func = model,
+                       parms = parms)
 
-# Declaramos los valores de los parametros
+                time <- c(out[,1])
+                MNA  <- c(out[,2])
+                MA   <- c(out[,3])
+                MNAF <- c(out[,4])
+                MAF  <- c(out[,5])
+                TL   <- c(out[,6])
+                TNA  <- c(out[,7])
+                TA   <- c(out[,8])
+                resp <- cbind(c(out[,1]), c(out[,2]), c(out[,3]), c(out[,4]),
+                              c(out[,5]), c(out[,6]), c(out[,7]), c(out[,8]))
 
-# aTl - Tasa de proliferacion de M. tuberculosis
-k1=3;
+                # print(resp)
 
-# aTna - Tasa de proliferacion de M. tuberculosis fagocitada por macrofago no activado
-k2=24;
-
-# aTa - Tasa de proliferacion de M. tuberculosis fagocitada por macrofago activado
-k3=0.625;
-
-# aM - Entrada constante de macrofago
-k4=0.1;
-
-# aMna - Tasa de reclutamiento de monocitos circulantes al lugar de la infección por Macrófago no activado que fagocitó
-k5=1;
-
-# aMa - Tasa de reclutamiento de monocitos circulantes al lugar de la infección por Macrófago activado que fagocitó 
-k6=3;
-
-# yMna - Capacidad fagocítica de Macrófago no activado
-k7=.75;
-
-# yMa - Capacidad fagocítica de Macrófago activado
-k8=0.35;
-
-# 0.650 uM - Activación de macrófago que no ha fagocitado
-k9=0.650;
-
-# uMf - Activación de macrófago que ha fagocitado
-k10=0.90;
-
-# dMna - Vida media de macrofago no activado
-k11=17;
-
-# dMnaTl - Tasa de muerte de M. tuberculosis libre causado por Macrófago no activado
-k12=48;
-
-# dTlMna - Tasa de muerte de Macrófago no activado causada por M. tuberculosis libre
-k13=2;
-
-# dMa - Vida media de macrofago activado
-k14=1;
-
-# dMaTl - Tasa de muerte de M. tuberculosis libre causado por Macrófago activado 
-k15=1;
-
-# dTlMa - Tasa de muerte de Macrófago activado causada por M. tuberculosis libre
-k16=2;
-
-# dMnaf - Vida media de Macrófago no activado que ha fagocitado
-k17=1;
-
-# dMnafTl - Tasa de muerte de M. tuberculosis libre causado por Macrófago no activado
-k18=12;
-
-# dMnafT - Tasa de muerte de M. tuberculosis fagocitada causado por Macrófago no activado
-k19=0.59;
-
-# dTlMnaf - Tasa de muerte de Macrófago no activado que ha fagocitado causada por M. tuberculosis libre
-k20=2;
-
-# 0.92 dTMnaf - Tasa de muerte de Macrófago no activado que ha fagocitado causada por M. tuberculosis fagocitada
-k21=0.92;
-
-# dMaf - Vida media de Macrófago activado que ha fagocitado
-k22=1.3;
-
-# dMafTl - Tasa de muerte de M. tuberculosis libre causado por Macrófago activado que ha fagocitado
-k23=5.3;
-
-# MafT - Tasa de muerte de M. tuberculosis fagocitada causado por Macrófago activado que ha fagocitado
-k24=0.34;
-
-# TlMaf - Tasa de muerte de Macrófago activado que ha fagocitado causada por M. tuberculosis libre
-k25=1.1;
-
-# TMaf - Tasa de muerte de Macrófago activado que ha fagocitado causada por M. tuberculosis fagocitada
-k26=19;
-
-# dTl - Vida media de M. tuberculosis libre
-k27=2.2;
-
-# dTna - Vida media de M. tuberculosis fagocitada por macrófago no activado
-k28=0.33;
-
-# dTa - Vida media de M. tuberculosis fagocitada por macrófago activado
-k29=7;
-
-
-parms=c(k1, k2, k3, k4, k5, k6, k7, k8, k9, k10, k11, k12, k13, k14, k15,
-        k16, k17, k18, k19, k20, k21, k22, k23, k24, k25, k26, k27, k28, k29);
-
-# condiciones iniciales de los nodos de la red
-y0=c(5,15,4,2,2,6,3);
-
-# Definimos el intervalo de integracion
-tspan =seq(from = 0, to = 12, by = 0.001)
-
-# Integramos con funcion ode
-out <- ode(y = y0, times = tspan, func = ModeloTuberculosis, parms = parms)
+                par(pty = "s")
+                plot(out[,1], out[,2], type = "l", col="pink", xlab = "Time",
+                     ylab ="[MNA:rosa,MA:rojo, MNAF:azul, MAF:negro, TL:verde,
+                     TNA: amarillo, TA:morado]")
+                lines(out[,1], out[,3], type = "l", col = "red")
+                lines(out[,1], out[,4], type = "l", col = "blue")
+                lines(out[,1], out[,5], type = "l", col = "black")
+                lines(out[,1], out[,6], type = "l", col = "green")
+                lines(out[,1], out[,7], type = "l", col = "yellow")
+                lines(out[,1], out[,8], type = "l", col = "purple")
+                legend = c("X(t)", "Y(t)", "XY(t)")
+                return(resp)
+            }
         ''')
 
+    run_f = ro.globalenv['run']
+    y0 = ro.IntVector([479, 380, 6, 3, 900, 8, 9])
+    k = ro.FloatVector([3, 24, 0.625, 0.1, 1, 3, 0.75, 0.35, 0.650, 0.90, 17,
+                        48, 2, 1, 1, 2, 1, 12, 0.59, 2, 0.92, 1.3, 5.3, 0.34,
+                        1.1, 19, 2.2, 0.33, 7])
+    output = run_f(y0, k)
+    matrix = np.array(output)  # ARRAY OF ARRAYS
 
-root = tk.Tk()
-root.title("Simulador de bacterias")
-root.geometry("300x150+500+300")
-app = Welcome(root)
-app.mainloop()
+    for data_set in matrix:
+        print(data_set)
+
+
+results = []
+# TODO: CHANGE FILE NAME DYNAMICALLY - ADD TO FUNCTION
+with open("input.csv") as csvfile:
+    # change contents to floats
+    reader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC)
+    for row in reader:  # each row is a list
+        for item in row:
+            results.append(item)
+# root = tk.Tk()
+# root.title("Simulador de bacterias")
+# root.geometry("300x150+500+300")
+# app = Welcome(root)
+# app.mainloop()
+create_r_script()
